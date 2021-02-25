b1786e3f60982c5f50f80796f637f2351d4edadf
Andrés Saucedo <snakescales@hotmail.com>
2021-02-24 21:48:14 -0800

    Added files to C

--- c_projects/special_char.c
+++ c_projects/special_char.c
@@ -4,14 +4,14 @@
 /*count lines in input */
 int main()
 {
-  int c, nl, t, b;
+  int c, nl, t, b, ch;
 /* defining baseline for variables newline, tab and blanks */
     nl = 0;
     t = 0;
     b = 0;
-
-printf("This program displays the newlines, tabs and blanks from input.\n");
-printf("To exit type Ctrl+d\n" );
+    ch = 0;
+printf("This program displays the newlines, tabs and blanks and characters from input.\n");
+printf("To exit press Ctrl+d\n" );
 printf("***************************************************************\n");
 
   while ((c = getchar()) != EOF)
@@ -29,9 +29,14 @@ else
         {
           ++b;
         }
+else
+      {
+        ++ch;
+      }
 
       printf("%i tabs\n", t);
       printf("%d newlines\n", nl);
       printf("%i blanks\n", b);
+      printf("%i characters\n", ch);
 return 0;
 }
