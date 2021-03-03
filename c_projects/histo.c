0192b8ad653f59507e7cfbe8c3833b57f65bbbd1
Andrés Saucedo <snakescales@hotmail.com>
2021-03-02 21:24:02 -0800

    Finished Histo!

--- c_projects/histo.c
+++ c_projects/histo.c
@@ -7,7 +7,9 @@ int wrdlng[HI];
 
 int main()
 {
-printf("This program tallies the number of characters in words from input:\n");
+printf("This program tallies the number of characters in words from input\n");
+printf("The maximum word length is set at %d characters \n",(HI - 1));
+
 c = i = cnt = ovrflo = 0; //Setting variables to 0
 for (i = 0; i < HI ; i++) {
    wrdlng[i]=0;
@@ -52,6 +54,22 @@ state = OUT;
 
     printf("\n overflow = %i", ovrflo);
     printf("\n");
+
+/////////////////////////start histogram /////////////////////
+int x;  //bar size counter
+x = 1;
+  printf("\n");
+  for (i = 0; i < HI; i++){ //set loop to go through array
+      if (i > 0){
+        printf("%2d :", i); //print labels
+      }
+    while(x <= wrdlng[i]) { //loop for printing histogram bar of wrdlng[i]
+      printf("#");
+      x++;
+    }
+    printf("\n");
+    x = 1;  //reset bar size counter, go to next array element
+  }
 
   return 0;
 }
