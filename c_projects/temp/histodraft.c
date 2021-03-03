9873c043c1a7730fc90bd49d276aeae97a7ed11a
Andrés Saucedo <snakescales@hotmail.com>
2021-03-03 11:13:02 -0800

    Updated temp folder

--- c_projects/temp/histodraft.c
+++ c_projects/temp/histodraft.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #define OUT 0                   //Defining variables
 #define IN 1
-#define HI 21
+#define HI 15
 int c, i, cnt, state, ovrflo;
 
 int wrdlng[HI];
 int main()
 {
 printf("This program tallies the number of characters in words from input:\n");
-c = i = cnt = ovrflo = 0;                  //Setting variables to 0
+printf("The maximum word length is set at %d characters \n",(HI - 1));
+
+c = i = cnt = ovrflo = 0;             //Setting variables to 0
 for (i = 0; i < HI ; i++) {
    wrdlng[i]=0;
 }
@@ -17,16 +19,15 @@ state = OUT;
   while ((c = getchar()) != EOF) {
 
     if (c != ' ' || c != '\t' || c != '\n') {
-
-        state = IN;
-        if (state = OUT){  //beginning of new word
-          ++cnt;
-        }else if (state = IN){
-          if (cnt >= HI) { //put characters over HI in overflow
+        if (state == OUT){  //beginning of new word
+          cnt = 1;
+          state = IN;
+        }else if (state == IN){ //in middle of a word
+            if (cnt >= HI) {
             ++ovrflo;
-          }else if (cnt < HI) { //if character count is less than HI add 1
+            }if (cnt < HI) {
             ++cnt;
-          }
+            }
         }
 
     }else if (c == ' ' || c == '\t' || c == '\n') {
@@ -36,7 +37,6 @@ state = OUT;
             state = OUT;
           }
           else {
-            state = OUT;
             cnt = 0;
           }
     }
@@ -54,6 +54,22 @@ state = OUT;
 
     printf("\n overflow = %i", ovrflo);
     printf("\n");
+
+    /////////////////////////start histogram /////////////////////
+    int x;  //bar size counter
+    x = 1;
+      printf("\n");
+      for (i = 0; i < HI; i++){ //set loop to go through array
+          if (i > 0){
+            printf("%2d :", i); //print labels
+          }
+        while(x <= wrdlng[i]) { //loop for printing histogram bar of wrdlng[i]
+          printf("#");
+          x++;
+        }
+        printf("\n");
+        x = 1;  //reset bar size counter, go to next array element
+      }
 
   return 0;
 }
