fd2d51c971012c7fa82b5ed6038a278f7c1c8cbe
Andrés Saucedo <snakescales@hotmail.com>
2021-03-13 22:43:15 -0800

    Complete detab and reverso

--- c_projects/reverso.c
+++ c_projects/reverso.c
@@ -2,43 +2,47 @@
 #define MAX 1000
 
 void reverso(char line[], char enil[]);
-int getl(char line, int index);
+int getl(char line[]);
 
+//this program gets a line from input, reverses it and outputs on screen
 int main(){
-int length,index;
+int length;
 char line[MAX];
 char enil[MAX];
-  while((length = getl(line,index)) > 0){
+
+  while(getl(line) > 0){
     reverso(line,enil);
-  printf("\n%s\n%s\n",line,enil);
+    printf("%s\n",enil);
   }
 
 return 0;
 }
 //copy array1 in reverse into array2
 void reverso(char line[], char enil[]){
-int i; int x=0;int length=0;
-  for(i=0; line[i] != '\0'; ++i)
+int i=0; int x=0; int length=0;
+
+  while(line[i] != '\0'){ //this section finds the first string length
+    i++;
+  }
   if(line[i] == '\0'){
     --i;
-    i = length;
+    length = i;
   }
   for(i=0, x=length; line[i] !='\0'; ++i, --x){
-    enil[x]=line[i];
-  }if(line[i]=='\0'){
+    enil[x]=line[i]; //copies first element of line1 to end of line2 etc.
+  }
+  if(line[i]=='\0'){ //when line1 ends, adds null character to end of line2
     enil[i]='\0';
   }
 }
 
 //function get a line from input and end it after a newline
-int getl(char line, int index){
+int getl(char line[]){
 int i, c;
 
   for(i=0; (c=getchar()) != EOF && c != '\n'; i++){
     line[i] = c;
   }if (c = '\n'){
-    line[i] = c;
-    i++;
     line[i] = '\0';
   }
 
