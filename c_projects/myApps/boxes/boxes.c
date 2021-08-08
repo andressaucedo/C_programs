6aa7feb8a0b274df27f3a5c84fd0fda7c9571411
A <super@pop-os.localdomain>
2021-08-08 10:41:44 -0700

    Updates to boxes

--- c_projects/myApps/boxes/boxes.c
+++ c_projects/myApps/boxes/boxes.c
@@ -1,7 +1,11 @@
 #include <raylib.h>
 int main(int argc, char const *argv[]) {
-
-    InitWindow(800, 800, "My Cool Program");
+    int winLength, winHeight, i,x;
+    int box;
+    winLength = 400;
+    winHeight = 400;
+    box = winHeight/8;
+    InitWindow(winLength, winHeight, "My Cool Program");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
@@ -9,11 +13,15 @@ int main(int argc, char const *argv[]) {
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You greated your first window!",
-        190,200,20,LIGHTGRAY);
+        DrawText("Congrats! You created your first window!",
+        50,200,14,LIGHTGRAY);
 
-        DrawRectangle(GetMouseX() - 50, GetMouseY() - 50, 100,100, RED);
+//        DrawRectangle(GetMouseX() - 50, GetMouseY() - 50, 100,100, RED);
 
+      for(i = 1, x =0; i <= 8; x += (box * 2)){
+        DrawRectangle(x, 0 , box, box ,RED);
+      //  x = i*box;
+      }
         EndDrawing();
     }
 
