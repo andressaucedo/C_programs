4eb63e587cd6ddcb0812d4462c33969ea95a7e03
andressaucedo <andressaucedo83@gmail.com>
2021-08-26 11:42:25 -0700

    Made booksave.c more modular, moved functions to separate file booksf.c

--- c_projects/c_primer/books/books.h
+++ c_projects/c_primer/books/books.h
@@ -1,5 +1,5 @@
 //header for use with booksave.c
-#define MAXWRD 40
+#define MAXWRD 128 //!!! edits will change struct size, make new .dat
 #define MAXBKS 10
 
 struct book {
--- c_projects/c_primer/books/booksf.c
+++ c_projects/c_primer/books/booksf.c
@@ -1,7 +1,73 @@
+#include <stdio.h>
+#include "books.h"
+/*
 int sortbooks(struct book *in, FILE *fp)
 {
     struct book alpha;
     rewind(fp);
     
 
+}
+
+/*function loop continues while we can read struct size chunks of data from our
+file where the book entries are stored*/
+int printdat(struct book biblio[MAXBKS], FILE *pbooks)
+{
+    extern int size;
+    int count = 0;
+    while (count < MAXBKS && fread(&biblio[count], size, 1, pbooks) == 1)
+    {
+        if (count == 0)
+        {
+            fputs("Current contents of book.dat:\n", stdout);
+        }
+        printf("%s  by:%s  $%.2f\n\n", biblio[count].title, biblio[count].author, biblio[count].price);
+        count++;
+    }
+
+    return count;
+    }
+
+int getbooks(struct book *biblio, int count)
+{
+    fputs("Please add Entries.\n"
+          "[Enter] to stop: \n"
+          "Book Title: ",
+          stdout);
+
+    while (count < MAXBKS && fgets(biblio[count].title, MAXWRD, stdin) != NULL && biblio[count].title[0] != '\0' && biblio[count].title[0] !=  '\n')
+    {
+        fputs("Author: ", stdout);
+        fgets(biblio[count].author, MAXWRD, stdin);
+        fputs("Price: ", stdout);
+        fscanf(stdin, "%f", &biblio[count].price);
+        while (getchar() != '\n')
+         ; //clean up trailing chars from fscanf
+        putchar('\n');
+        count++;
+        if (count < MAXBKS)
+        {
+            fputs("Book Title: ", stdout);
+        }
+    }
+return count;
+}
+
+/*function to get a YES(1) or NO(0) or OTHER(2) answer */
+int yesno(void)
+{
+    int num = 20;
+    char yesno[num];
+    char yn;
+    fgets(yesno, num, stdin);
+    sscanf(yesno, "%c", &yn);
+    if (yn == 'y' || yn == 'Y')
+    {
+        return 1;
+    }
+    else if (yn == 'n' || yn == 'N')
+    {
+        return 0;
+    }else
+        return 2;
 }
